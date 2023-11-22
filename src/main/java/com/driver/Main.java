658416ea629a605715ebca78ff548e9ef2a3783c
Samiksha Sankar <samikshasankar2000@gmail.com>
2023-11-22 16:33:01 +0530

    oops

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly t = new RWOnly();
+
+      t.setA("Samiksha");
+      System.out.print(t.getB());
+
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+      public void setA(String b) {
+          name = b;
+      }
+      public String getB() {
+          return name;
+      }
+
 }
